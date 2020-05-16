1fe5b780e0bc5b57d37ea0d5bad6e7cecdbb84f1
Olumide <volumide42@gmai.com>
2020-05-16 19:18:52 +0100

    starting delight ambassador website

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  public isShow:boolean
+  topPosToStartShowing = 200
+  constructor() { }
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    let check = document.getElementById('link').getBoundingClientRect().top
+    if (check === 0) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  scrollToview(target:string){
+    let element = document.getElementById(target)
+    let top = element.getBoundingClientRect().top
+    window.scrollTo({
+      top: top + window.pageYOffset - 100,
+      left: 0,
+      behavior: "smooth"
+    })
+  }
+
+  ngOnInit() {
+  }
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'navigation', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'about', component: AboutComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
